9d0f9e8b36707b98307421b15a7967a23973f400
Thiago Ritcher <thiagoritcher@gmail.com>
2025-10-28 21:24:40 -0300

    Added all

--- src/main/java/br/com/ritcher/sistema/seguranca/controller/UserAdd.java
+++ src/main/java/br/com/ritcher/sistema/seguranca/controller/UserAdd.java
@@ -1,10 +1,23 @@
 package br.com.ritcher.sistema.seguranca.controller;
 
+import br.com.ritcher.sistema.seguranca.gen.data.User;
+import br.com.ritcher.sistema.seguranca.gen.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/user")
 public class UserAdd {
-	
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/thiago")
+    public Flux<User> getAllUsersThiago(){
+            return userService
+                    .findAll()
+                    .filter(u -> u.getUsername().contains("thiago"));
+    }
 }
