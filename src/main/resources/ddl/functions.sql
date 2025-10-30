92aca0c17de25759adb4bea12fbd134fc1b9cb71
Thiago Ritcher <thiagoritcher@gmail.com>
2025-10-30 11:01:52 -0300

    Set id function

--- src/main/resources/ddl/functions.sql
+++ src/main/resources/ddl/functions.sql
@@ -12,8 +12,8 @@ create function rdnid() returns integer
   -- max int
   return random() * (2147483646) + 1;
 
-create function setid(tbl text) returns integer
+create function setid(tbl text, id integer) returns integer
   language sql
   immutable
   returns null on null input
-  return setval(tbl || '_id_seq', 21, true);
+  return setval(tbl || '_id_seq', id, true);
